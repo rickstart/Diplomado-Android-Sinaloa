ae66d384ad484278a677b003f3f883346bf494e3
rickstart <ricardo.celj@gmail.com>
2015-05-05 12:27:03 -0500

    Make a Sum with two operators

--- Calculator/app/src/main/java/com/mobintum/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/mobintum/calculator/MainActivity.java
@@ -1,17 +1,71 @@
 package com.mobintum.calculator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button btnZero,btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine;
+    Button btnAC, btnPlusLess, btnPercent, btnDivision, btnMultiply, btnLess, btnPlus, btnEqual, btnDot ;
+    TextView textResult;
+
+    Double oper1=0.0, oper2=0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textResult = (TextView) findViewById(R.id.textResult);
+
+        btnZero = (Button) findViewById(R.id.btnZero);
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnThree = (Button) findViewById(R.id.btnThree);
+        btnFour = (Button) findViewById(R.id.btnFour);
+        btnFive = (Button) findViewById(R.id.btnFive);
+        btnSix = (Button) findViewById(R.id.btnSix);
+        btnSeven = (Button) findViewById(R.id.btnSeven);
+        btnEight = (Button) findViewById(R.id.btnEight);
+        btnNine = (Button) findViewById(R.id.btnNine);
+        btnAC= (Button) findViewById(R.id.btnAC);
+        btnPlusLess = (Button) findViewById(R.id.btnPlusLess);
+        btnPercent = (Button) findViewById(R.id.btnPercent);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnLess = (Button) findViewById(R.id.btnLess);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnEqual = (Button) findViewById(R.id.btnEqual);
+        btnDot = (Button) findViewById(R.id.btnDot);
+
+        btnZero.setOnClickListener(this);
+        btnOne.setOnClickListener(this);
+        btnTwo.setOnClickListener(this);
+        btnThree.setOnClickListener(this);
+        btnFour.setOnClickListener(this);
+        btnFive.setOnClickListener(this);
+        btnSix.setOnClickListener(this);
+        btnSeven.setOnClickListener(this);
+        btnEight.setOnClickListener(this);
+        btnNine.setOnClickListener(this);
+        btnAC.setOnClickListener(this);
+        btnPlusLess.setOnClickListener(this);
+        btnPercent.setOnClickListener(this);
+        btnDivision.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnLess.setOnClickListener(this);
+        btnPlus.setOnClickListener(this);
+        btnEqual.setOnClickListener(this);
+        btnDot.setOnClickListener(this);
+
+
     }
 
 
@@ -35,5 +89,64 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.btnAC:
+                textResult.setText("");
+                oper1=0.0;
+                oper2=0.0;
+                break;
+
+            case R.id.btnPlus:
+
+                Log.e("OPERATION", "OPER1 "+oper1+" OPER2 "+oper2);
+                if(oper1==0.0){
+                    oper1 = Double.valueOf(textResult.getText().toString());
+                    Log.e("OPERATION1", "OPER1 "+oper1+" OPER2 "+oper2);
+                    return;
+                }
+                if(oper1!=0.0){
+                    oper2 = Double.valueOf(textResult.getText().toString());
+                    Log.e("OPERATION2", "OPER1 "+oper1+" OPER2 "+oper2);
+
+                }
+
+                if(oper2!=0.0){
+                    Double operation = oper1+oper2;
+                    textResult.setText(operation.toString());
+                    oper1=operation;
+                    oper2=0.0;
+                    Log.e("OPERATION", ""+operation);
+                    return;
+
+                }
+
+
+
+
+                break;
+
+
+
+            default:
+                Button btnTemp = (Button) findViewById(v.getId());
+                if(oper1==0.0){
+                    textResult.append(btnTemp.getText().toString());
+
+
+                }else{
+                    textResult.setText(btnTemp.getText().toString());
+                }
+
+                break;
+
+        }
+
     }
 }
